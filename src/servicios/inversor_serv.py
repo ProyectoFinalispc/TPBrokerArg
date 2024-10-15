66bc5dcd70394d09fc8d3e7ad948389c9403ceb5
DESKTOP-BTMSFSP\Yoh <emiliosoad@gmail.com>
2024-10-14 22:32:12 -0300

    se cambia ruta VM para evitar errores

---
+++ src/servicios/inversor_serv.py
@@ -0,0 +1,10 @@
+from repositorio.inversorRepository import InversorRepository
+
+class InversorService:
+    inversorRepository = InversorRepository()
+
+    def consultar_inversor (self, dni):
+        return self.inversorRepository.obtener_inversor(dni)
+
+    def crear_inversor_serv (self, inversor_nuevo):
+        return self.inversorRepository.crear_inversor(inversor_nuevo)
